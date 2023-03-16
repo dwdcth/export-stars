d216be6a439492a3511ae0e0d3ddacab07cf9961
banshan <dwdcth@163.com>
2023-03-16 17:32:45 +0800

    Update export_stars.py

--- export_stars/export_stars.py
+++ export_stars/export_stars.py
@@ -31,7 +31,7 @@ def config_retry(backoff_factor=1.0, total=8):
 def parse_args():
     parser = ArgumentParser(description="export a GitHub user's starred repositorys to CSV")
     parser.add_argument("--user")
-    parser.add_argument("--github-token")
+    parser.add_argument("--token")
     return parser.parse_args()
 
 
